302fcfe866b5a956ff3f69972e9f3f56c00d176d
Administrator <mha263@udml10.local>
2018-10-20 03:29:45 -0600

    two board no functionality so far

---
+++ ToDoList/ToDoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoTableViewCell.swift
+//  ToDoList
+//
+//  Created by Md. Shahadet Hossain on 2018-10-20.
+//  Copyright © 2018 Md. Shahadet Hossain. All rights reserved.
+//
+
+import UIKit
+
+//
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func OpenCameraToTakePhoto(_ sender: Any) {
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imageTakingController = UIImagePickerController()
+        
+        //check if the device has a camera or not
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            
+            let alertController = UIAlertController.init(title: nil, message: "Device has no camera.", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction.init(title: "Alright", style: .default, handler: {(alert: UIAlertAction!) in
+            })
+            
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+            imageTakingController.sourceType = .photoLibrary
+            
+            
+        }
+        else{
+            imageTakingController.sourceType = .camera
+            
+            imageTakingController.delegate = self
+            present(imageTakingController, animated: true, completion: nil)
+            
+            print("SH:: Test")
+        }
+    }
 }
 
